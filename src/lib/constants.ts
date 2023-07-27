c16e77c188ee36f1cc8971a0826e9ff92372a842
Andrii Babushko <andriibabushko@gmail.com>
2023-07-27 12:42:42 +0300

    Add new page paths

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,7 @@ export const pages = {
   signIn: "/auth/signin/",
   signUp: "/auth/signup/",
   signOut: "/auth/signout/",
+  premium: "/premium/",
+  help: "/help/",
+  download: "/download/",
 } as const;
