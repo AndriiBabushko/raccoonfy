da64f0480fd58aa9716b2eb18151068ac7c496f5
Andrii Babushko <andriibabushko@gmail.com>
2023-07-29 16:57:16 +0300

    Try to fix deploy api next auth error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -60,5 +60,4 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
